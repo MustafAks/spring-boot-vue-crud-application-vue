be2cfe4f25a22f5667e31d4efc04aeaf855c675b
mustafaak <mustafa.aksu@nurd.com>
2020-02-14 17:37:13 +0300

    first commit

---
+++ src/components/ListCoursesComponent.vue
@@ -0,0 +1,22 @@
+<script>
+    import CourseDataService from '../service/CourseDataService';
+    export default {
+        name: "CoursesList",
+        data() {
+            return {
+                INSTRUCTOR: "in28minutes"
+            };
+        },
+        methods: {
+            refreshCourses() {
+                CourseDataService.retrieveAllCourses(this.INSTRUCTOR) //HARDCODED
+                    .then(response => {
+                        console.log(response.data);
+                    });
+            }
+        },
+        created() {
+            this.refreshCourses();
+        }
+    };
+</script>
