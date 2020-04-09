15f349daba40d01716152ad891765b26ba3f14d9
Cengizhan Ozcan <cengizhan_ozcan@hotmail.com>
2020-04-09 17:08:25 +0300

    router authorization check added.

--- src/router.js
+++ src/router.js
@@ -22,6 +22,7 @@ import SubscriberEndDateControl from "./views/subscriber/SubscriberEndDateContro
 import Login from "./views/user/Login";
 import AddNewspaper from "./views/subscriber/AddNewspaper";
 import AddPages from "./views/subscriber/AddPages";
+import Page401 from "./views/Page401";
 
 
 Vue.component('nav-bar', NavBar);
@@ -33,61 +34,96 @@ Vue.use(axios);
 Vue.use(errorHandler);
 Vue.use(notificationUtils);
 
-export default new Router({
+const router = new Router({
     routes: [
         {
             path: '/',
             name: 'login',
             component: Login,
-            meta: { title: 'Giriş Yap' }
+            meta: { title: 'Giriş Yap', isPublic: true }
         },
         {
             path: '/mainPage',
             name: 'mainPage',
             component: MainPage,
-            meta: { title: 'Hasret Gazetesi - Kemaliye' }
+            meta: { title: 'Hasret Gazetesi - Kemaliye', roles: ["ROLE_USER", "ROLE_ADMIN"] }
         },
         {
             path: '/register',
             name: 'Register',
             component: Register,
-            meta: { title: 'Yeni Abone' }
+            meta: { title: 'Yeni Abone', roles: ["ROLE_ADMIN"] }
         },
         {
             path: '/addNewspaper',
             name: 'AddNewspaper',
             component: AddNewspaper,
-            meta: { title: 'Yeni Gazete Ekle' }
+            meta: { title: 'Yeni Gazete Ekle', roles: ["ROLE_ADMIN"] }
         },
         {
             path: '/addPages',
             name: 'AddPages',
             component: AddPages,
-            meta: { title: 'Yeni Sayfa Ekle' }
+            meta: { title: 'Yeni Sayfa Ekle', roles: ["ROLE_ADMIN"] }
         },
         {
             path: '/filter',
             name: 'FilterList',
             component: FilterList,
-            meta: { title: 'Filtrele' }
+            meta: { title: 'Filtrele', roles: ["ROLE_ADMIN"]  }
         },
         {
             path: '/about',
             name: 'About',
             component: About,
-            meta: { title: 'Hakkımızda' }
+            meta: { title: 'Hakkımızda', isPublic: true }
         },
         {
             path: '/update',
             name: 'Update',
             component: Update,
-            meta: { title: 'Güncelle' }
+            meta: { title: 'Güncelle', roles: ["ROLE_ADMIN"]  }
         },
         {
             path: '/subscriberEndDateControl',
             name: 'SubscriberEndDateControl',
             component: SubscriberEndDateControl,
-            meta: {title: 'Abone Bitis'}
+            meta: {title: 'Abone Bitis', roles: ["ROLE_ADMIN"] }
+        },
+        {
+          path: '/401',
+          name: '401',
+          component: Page401,
+          meta: {title: 'Yetki PRoblemi', isPublic: true }
         }
     ]
-})
+});
+
+router.beforeEach((to, from, next) => {
+    const isPublic = to.matched.some(record => record.meta.isPublic);
+    const roles = to.meta.roles;
+    const user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user'))  : null;
+    const userRole = user ? user.role : null;
+
+    if(isPublic){
+        next();
+    }
+    else if (!userRole) {
+        return next({
+            path:'/',
+            query: {redirect: to.fullPath}
+        });
+    }
+
+    else if(roles && !roles.includes(userRole)){
+        return next({
+            path:'/401',
+            query: {redirect: to.fullPath}
+        });
+    }
+    else {
+        next();
+    }
+});
+
+export default router;
---
+++ src/views/Page401.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="page401">
+    <h1>Bu sayfayı görüntüleme yetkiniz bulunmamaktadır!</h1>
+  </div>
+</template>
