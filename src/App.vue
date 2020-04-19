8be8af196e43502c925782f60c38fdebd90ba7aa
onurgo <onurgokberk.ozerdal@nurd.com>
2020-04-19 14:11:46 +0300

    footer changes.

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,17 @@
             <router-view/>
         </div>
         <div id='footer-view' class="text-center">
-            <b-img-lazy v-bind="mainProps"
+            <b-img-lazy v-bind="mainProps" style="margin: 0 2.5px 0 0"
                     :src="require('./assets/images/FooterImages/facebook.png')"
                     onclick="window.open('https://www.facebook.com/hasret.kemaliye','facebook')" ></b-img-lazy>
-            <b-img-lazy v-bind="mainProps"
+            <b-img-lazy v-bind="mainProps" style="margin: 0 2.5px 0 2.5px"
                     :src="require('./assets/images/FooterImages/twitter.png')"
                     onclick="window.open('https://twitter.com/HasretKemaliye','twitter')" ></b-img-lazy>
-            <b-img-lazy v-bind="mainProps"
+            <b-img-lazy v-bind="mainProps" style="margin: 0 0 0 2.5px"
                     :src="require('./assets/images/FooterImages/instagram.png')"
                     onclick="window.open('https://www.instagram.com/hasretgazetesi/','instagram')" ></b-img-lazy>
           <div class="container">
-                <p class="text-center text-white">©{{ new Date().getFullYear() }} Copyright — Kemaliye</p>
+                <p class="text-center text-white">©{{ new Date().getFullYear() }} Copyright — Kemaliye Hasret Gazetesi</p>
             </div>
 
         </div>
