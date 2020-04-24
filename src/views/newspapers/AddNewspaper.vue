29e9b21d2e799091f96e384482c80b16b431b10a
Beyza Ekici <beyza.ekici@hotmail.com>
2020-04-25 02:34:10 +0300

    gazete ekle sayfası düzenleme

--- src/views/newspapers/AddNewspaper.vue
+++ src/views/newspapers/AddNewspaper.vue
@@ -1,68 +1,129 @@
+<!--<template>-->
+<!--  <div class="container">-->
+<!--    <div class="center">-->
+<!--      <b-container class="bv-example-row">-->
+<!--        <b-row>-->
+<!--          <b-col>-->
+<!--            <h3>Gazete Ekle</h3>-->
+<!--            <b-row>-->
+<!--              <b-col sm="4">-->
+<!--                <label>Başlık:</label>-->
+<!--              </b-col>-->
+<!--              <b-col sm="7">-->
+<!--                <ValidationProvider name="newspaperTitle" rules="required">-->
+<!--                  <b-form-group slot-scope="{ valid, errors }">-->
+<!--                <b-form-input v-model="newspaper.title" :state='errors[0] ? false : (valid ? true : null)'></b-form-input>-->
+<!--                    <b-form-invalid-feedback>-->
+<!--                      {{ errors[0] }}-->
+<!--                    </b-form-invalid-feedback>-->
+<!--                  </b-form-group>-->
+<!--                </ValidationProvider>-->
+<!--              </b-col>-->
+<!--            </b-row>-->
+<!--            <b-row>-->
+<!--              <b-col sm="4">-->
+<!--                <label>Sayı:</label>-->
+<!--              </b-col>-->
+<!--              <b-col sm="7">-->
+<!--                <ValidationProvider name="newspaperTitle" rules="required">-->
+<!--                  <b-form-group slot-scope="{ valid, errors }">-->
+<!--                <b-form-input v-model="newspaper.issue" type = 'number' :min="1" :state='errors[0] ? false : (valid ? true : null)'></b-form-input>-->
+<!--                    <b-form-invalid-feedback>-->
+<!--                      {{ errors[0] }}-->
+<!--                    </b-form-invalid-feedback>-->
+<!--                  </b-form-group>-->
+<!--                </ValidationProvider>-->
+<!--              </b-col>-->
+<!--            </b-row>-->
+<!--            <b-row>-->
+<!--              <b-col sm="4">-->
+<!--                <label>Yıl:</label>-->
+<!--              </b-col>-->
+<!--              <b-col sm="7">-->
+<!--                <ValidationProvider name="newspaperTitle" rules="required">-->
+<!--                  <b-form-group slot-scope="{ valid, errors }">-->
+<!--                <b-form-input v-model="newspaper.year" type = 'number' :min="2010"  :state='errors[0] ? false : (valid ? true : null)'></b-form-input>-->
+<!--                    <b-form-invalid-feedback>-->
+<!--                      {{ errors[0] }}-->
+<!--                    </b-form-invalid-feedback>-->
+<!--                  </b-form-group>-->
+<!--                </ValidationProvider>-->
+<!--              </b-col>-->
+<!--            </b-row>-->
+
+<!--            <b-container class="bv-example-row">-->
+<!--              <b-row align-h="end">-->
+<!--                <b-col cols="3">-->
+<!--                  <b-button type="submit" class="w-75" variant="primary" v-on:click.prevent="createNewspaper">Kaydet</b-button>-->
+<!--                </b-col>-->
+<!--              </b-row>-->
+<!--            </b-container>-->
+<!--          </b-col>-->
+<!--        </b-row>-->
+<!--      </b-container>-->
+<!--    </div>-->
+
+<!--    <b-container class="bv-example-row">-->
+<!--      <b-row>-->
+<!--        <b-col>-->
+<!--          <div class="container">-->
+<!--            <h3>Gazeteler</h3>-->
+<!--            <div class="container">-->
+<!--              <div>-->
+<!--                <b-table :fields="newspapersFields" :items="newspapersItems">-->
+<!--                  <template v-slot:cell(operation)="row">-->
+<!--                    <b-row>-->
+<!--                      <b-button variant="success" v-on:click.prevent="redirectPages(row.item)">Sayfa Ekle/Çıkar</b-button>-->
+<!--                      <b-button variant="danger"  v-on:click.prevent="deleteNewspaper(row.item.id)">Sil</b-button>-->
+<!--                    </b-row>-->
+<!--                  </template>-->
+<!--                </b-table>-->
+<!--              </div>-->
+<!--            </div>-->
+<!--          </div>-->
+<!--        </b-col>-->
+<!--      </b-row>-->
+<!--    </b-container>-->
+<!--  </div>-->
+<!--</template>-->
+
 <template>
-  <div class="container">
-    <div class="center">
-      <b-container class="bv-example-row">
-        <b-row>
-          <b-col>
-            <h3>Gazete Ekle</h3>
-            <b-row>
-              <b-col sm="4">
-                <label>Başlık:</label>
-              </b-col>
-              <b-col sm="7">
-                <ValidationProvider name="newspaperTitle" rules="required">
-                  <b-form-group slot-scope="{ valid, errors }">
-                <b-form-input v-model="newspaper.title" :state='errors[0] ? false : (valid ? true : null)'></b-form-input>
-                    <b-form-invalid-feedback>
-                      {{ errors[0] }}
-                    </b-form-invalid-feedback>
-                  </b-form-group>
-                </ValidationProvider>
-              </b-col>
-            </b-row>
-            <b-row>
-              <b-col sm="4">
-                <label>Sayı:</label>
-              </b-col>
-              <b-col sm="7">
-                <ValidationProvider name="newspaperTitle" rules="required">
-                  <b-form-group slot-scope="{ valid, errors }">
-                <b-form-input v-model="newspaper.issue" type = 'number' :min="1" :state='errors[0] ? false : (valid ? true : null)'></b-form-input>
-                    <b-form-invalid-feedback>
-                      {{ errors[0] }}
-                    </b-form-invalid-feedback>
-                  </b-form-group>
-                </ValidationProvider>
-              </b-col>
-            </b-row>
-            <b-row>
-              <b-col sm="4">
-                <label>Yıl:</label>
-              </b-col>
-              <b-col sm="7">
-                <ValidationProvider name="newspaperTitle" rules="required">
-                  <b-form-group slot-scope="{ valid, errors }">
-                <b-form-input v-model="newspaper.year" type = 'number' :min="2010"  :state='errors[0] ? false : (valid ? true : null)'></b-form-input>
-                    <b-form-invalid-feedback>
-                      {{ errors[0] }}
-                    </b-form-invalid-feedback>
-                  </b-form-group>
-                </ValidationProvider>
-              </b-col>
-            </b-row>
+    <div class="container">
+      <div class="center">
+    <b-form @submit="createNewspaper" @reset="onReset">
+      <b-form-group
+              id="input-group-1"
+              label="Başlık:"
+              label-for="newspaperTitle"
+      >
+        <b-form-input
+                id="newspaperTitle"
+                v-model="newspaper.title"
+                required
+        ></b-form-input>
+      </b-form-group>
 
-            <b-container class="bv-example-row">
-              <b-row align-h="end">
-                <b-col cols="3">
-                  <b-button type="submit" class="w-75" variant="primary" v-on:click.prevent="createNewspaper">Kaydet</b-button>
-                </b-col>
-              </b-row>
-            </b-container>
-          </b-col>
-        </b-row>
-      </b-container>
-    </div>
+      <b-form-group id="input-group-2" label="Sayı:" label-for="newspaperIssue">
+        <b-form-input
+                id="newspaperIssue"
+                v-model="newspaper.issue"
+                type="number"
+                required
+        ></b-form-input>
+      </b-form-group>
 
+      <b-form-group id="input-group-3" label="Yıl:" label-for="newspaperYear">
+        <b-form-input
+                id="newspaperYear"
+                v-model="newspaper.year"
+                type="number"
+                required
+        ></b-form-input>
+      </b-form-group>
+      <b-button type="submit" variant="success">Ekle</b-button>
+      <b-button type="reset" variant="primary">Reset</b-button>
+    </b-form>
+<div>
     <b-container class="bv-example-row">
       <b-row>
         <b-col>
@@ -85,15 +146,12 @@
       </b-row>
     </b-container>
   </div>
+    </div>
+    </div>
 </template>
-
 <script>
   import NewspaperService from "../../service/NewspaperService";
-  import {ValidationProvider} from "vee-validate";
   export default {
-    components: {
-      ValidationProvider
-    },
     data(){
       return {
         newspaper: {
@@ -146,10 +204,22 @@
       async loadNewspapers() {
         this.newspapersItems = await NewspaperService.list();
       },
-
       redirectPages(params) {
         this.$router.push({ name: 'AddPages', params : params});
       },
+      onReset(evt) {
+        evt.preventDefault()
+        // Reset our form values
+        this.newspaper.title = '',
+                this.newspaper.id = '',
+                this.newspaper.issue = '',
+                this.newspaper.year = '',
+                // Trick to reset/clear native browser form validation state
+                this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      }
     },
     beforeMount() {
       this.loadNewspapers();
