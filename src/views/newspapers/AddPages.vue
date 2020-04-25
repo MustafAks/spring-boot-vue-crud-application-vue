b9f3dd0cf91b319aca1ff1609b268b1d8d68a7fd
Beyza Ekici <beyza.ekici@hotmail.com>
2020-04-25 15:20:08 +0300

    sayfa ekle sayfası düzenleme

--- src/views/newspapers/AddPages.vue
+++ src/views/newspapers/AddPages.vue
@@ -1,88 +1,143 @@
+<!--<template>-->
+<!--  <div class="container">-->
+<!--    <div class="center">-->
+<!--    <b-container class="bv-example-row">-->
+<!--      <b-row>-->
+<!--        <b-col>-->
+<!--          <h3>{{ newspaper.issue}} sayı numaralı gazete ({{ newspaper.year}})</h3>-->
+<!--          <h3>Sayfa Ekle</h3>-->
+<!--          <b-row>-->
+<!--            <b-col sm="4">-->
+<!--              <label>Sayfa No:</label>-->
+<!--            </b-col>-->
+<!--            <b-col sm="7">-->
+<!--              <ValidationProvider name="pageNumber" rules="required">-->
+<!--                <b-form-group slot-scope="{ valid, errors }">-->
+<!--              <b-form-input v-model="page.pageNumber" type = 'number' :state='errors[0] ? false : (valid ? true : null)' :min="0"  ></b-form-input>-->
+<!--                  <b-form-invalid-feedback>-->
+<!--                    {{ errors[0] }}-->
+<!--                  </b-form-invalid-feedback>-->
+<!--                </b-form-group>-->
+<!--              </ValidationProvider>-->
+<!--            </b-col>-->
+<!--          </b-row>-->
+<!--          <b-row>-->
+<!--            <b-col sm="4">-->
+<!--              <label>Dosya Ekle:</label>-->
+<!--            </b-col>-->
+<!--            <b-col sm="7">-->
+<!--              <ValidationProvider name="file" rules="required">-->
+<!--                <b-form-group-->
+<!--                        slot-scope="{ valid, errors }">-->
+<!--              <b-form-file v-model="page.file" class="mt-3" plain :state="errors[0] ? false : valid"></b-form-file>-->
+<!--                <b-form-invalid-feedback>-->
+<!--                  {{ errors[0] }}-->
+<!--                </b-form-invalid-feedback>-->
+<!--              </b-form-group>-->
+<!--              </ValidationProvider>-->
+<!--            </b-col>-->
+<!--          </b-row>-->
+
+<!--          <b-container class="bv-example-row">-->
+<!--            <b-row align-h="end">-->
+<!--              <b-col cols="3">-->
+<!--                <b-button class="w-75" variant="primary" v-on:click.prevent="createPage">Kaydet</b-button>-->
+<!--              </b-col>-->
+<!--            </b-row>-->
+<!--          </b-container>-->
+<!--          <b-col >-->
+<!--            <div class="container">-->
+<!--              <h3>Sayfalar</h3>-->
+<!--              <div class="container">-->
+<!--                <div>-->
+<!--                  <b-table :fields="pagesFields" :items="pagesItems">-->
+<!--                    <template v-slot:cell(file)="row">-->
+<!--                      <b-row>-->
+<!--                        <b-button variant="success" v-on:click.prevent="previewPage(row.item.id)">Sayfa Önizleme</b-button>-->
+<!--                      </b-row>-->
+<!--                    </template>-->
+<!--                      <template v-slot:cell(operations)="row">-->
+<!--                      <b-row>-->
+<!--                        <b-button variant="danger" v-on:click.prevent="deletePage(row.item.id)">Sayfa Sil</b-button>-->
+<!--                      </b-row>-->
+<!--                    </template>-->
+<!--                  </b-table>-->
+<!--                </div>-->
+<!--                <div>-->
+<!--                </div>-->
+<!--              </div>-->
+<!--            </div>-->
+<!--          </b-col>-->
+<!--        </b-col>-->
+<!--      </b-row>-->
+<!--    </b-container>-->
+<!--  </div>-->
+<!--  </div>-->
+<!--</template>-->
 <template>
   <div class="container">
     <div class="center">
-    <b-container class="bv-example-row">
-      <b-row>
-        <b-col>
-          <h3>{{ newspaper.issue}} sayı numaralı gazete ({{ newspaper.year}})</h3>
-          <h3>Sayfa Ekle</h3>
-          <b-row>
-            <b-col sm="4">
-              <label>Sayfa No:</label>
-            </b-col>
-            <b-col sm="7">
-              <ValidationProvider name="pageNumber" rules="required">
-                <b-form-group slot-scope="{ valid, errors }">
-              <b-form-input v-model="page.pageNumber" type = 'number' :state='errors[0] ? false : (valid ? true : null)' :min="0"  ></b-form-input>
-                  <b-form-invalid-feedback>
-                    {{ errors[0] }}
-                  </b-form-invalid-feedback>
-                </b-form-group>
-              </ValidationProvider>
-            </b-col>
-          </b-row>
-          <b-row>
-            <b-col sm="4">
-              <label>Dosya Ekle:</label>
-            </b-col>
-            <b-col sm="7">
-              <ValidationProvider name="file" rules="required">
-                <b-form-group
-                        slot-scope="{ valid, errors }">
-              <b-form-file v-model="page.file" class="mt-3" plain :state="errors[0] ? false : valid"></b-form-file>
-                <b-form-invalid-feedback>
-                  {{ errors[0] }}
-                </b-form-invalid-feedback>
-              </b-form-group>
-              </ValidationProvider>
-            </b-col>
-          </b-row>
-
-          <b-container class="bv-example-row">
-            <b-row align-h="end">
-              <b-col cols="3">
-                <b-button class="w-75" variant="primary" v-on:click.prevent="createPage">Kaydet</b-button>
-              </b-col>
-            </b-row>
-          </b-container>
-          <b-col >
-            <div class="container">
-              <h3>Sayfalar</h3>
-              <div class="container">
-                <div>
-                  <b-table :fields="pagesFields" :items="pagesItems">
-                    <template v-slot:cell(file)="row">
-                      <b-row>
-                        <b-button variant="success" v-on:click.prevent="previewPage(row.item.id)">Sayfa Önizleme</b-button>
-                      </b-row>
-                    </template>
-                      <template v-slot:cell(operations)="row">
-                      <b-row>
-                        <b-button variant="danger" v-on:click.prevent="deletePage(row.item.id)">Sayfa Sil</b-button>
-                      </b-row>
-                    </template>
-                  </b-table>
-                </div>
-                <div>
-                </div>
-              </div>
-            </div>
-          </b-col>
-        </b-col>
-      </b-row>
-    </b-container>
-  </div>
+      <b-form @submit="createPage" @reset="clear">
+        <b-form-group
+                id="input-group-1"
+                label-cols-sm="4"
+                label-cols-lg="3"
+                label="Sayfa No :"
+                label-for="pageNumber"
+        >
+          <b-form-input
+                  id="pageNumber"
+                  v-model="page.pageNumber"
+                  required
+                  type = 'number'
+          ></b-form-input>
+        </b-form-group>
+        <b-form-group
+                id="input-group-2"
+                label-cols-sm="4"
+                label-cols-lg="3"
+                label="Dosya Ekle :"
+                label-for="file"
+                :min=0
+        >
+          <b-form-file id="file"
+                        v-model="page.file"
+                       class="mt-3"
+                        plain
+                       required
+          >
+          </b-form-file>
+        </b-form-group>
+        <b-row align-h="end">
+          <b-button type="submit" variant="success">
+            Ekle
+          </b-button>
+          <b-button type="reset" variant="primary" style="margin-left: 5px">Temizle</b-button>
+        </b-row>
+      </b-form>
+    </div>
+    <h3 align="center" style="margin-top: 50px">Sayfalar</h3>
+    <div class="container" style="text-align: center">
+      <b-table :fields="pagesFields" :items="pagesItems">
+        <template v-slot:cell(operation)="row">
+          <b-button variant="success" v-on:click.prevent="previewPage(row.item.id)">
+            <b-icon icon="layout-text-sidebar-reverse"></b-icon>
+            Sayfalar
+          </b-button>
+          <b-button style="margin-left: 5px" variant="danger"
+                    v-on:click.prevent="deletePage(row.item.id)">
+            <b-icon icon="trash-fill"></b-icon>
+            Sil
+          </b-button>
+        </template>
+      </b-table>
+    </div>
   </div>
 </template>
-
 <script>
   import NewspaperService from "../../service/NewspaperService";
   import GeneratePdfUtils from "../../utils/GeneratePdfUtils";
-  import { ValidationProvider } from 'vee-validate';
   export default {
-    components: {
-      ValidationProvider
-    },
     data(){
       return {
         newspaper: {
@@ -130,10 +185,8 @@
         formData.append('newspaperId',newspaperId);
         await NewspaperService.savePage(formData);
         this.$notification(this, 'Sayfa başarılı bir şekilde kayıt edildi.');
+        this.clear();
         this.loadPages(this.newspaper.id);
-        //clear form data
-        this.page.pageNumber = null;
-        this.page.file = null;
       },
 
       async loadPages(newspaperId) {
@@ -148,6 +201,13 @@
         this.$notification(this, 'Sayfa başarılı bir şekilde silindi.');
         this.loadPages(this.newspaper.id);
       },
+      clear() {
+        // Reset our form values
+        this.page.id = '';
+        this.page.pageNumber = '';
+        this.page.file = '';
+        this.page.newspapers = '';
+      }
     },
     beforeMount() {
       if (this.newspaper.id === undefined || this.newspaper.id === null) {
