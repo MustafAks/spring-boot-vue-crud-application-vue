9f122b84b593ec0749f69ceb92dc0664ed25bc9b
Beyza Ekici <beyza.ekici@hotmail.com>
2020-05-02 15:43:50 +0300

    profile

--- src/views/subscribers/UserProfile.vue
+++ src/views/subscribers/UserProfile.vue
@@ -2,14 +2,9 @@
   <div class="container">
       <b-row>
         <b-col class="col-6">
-          <b-form-group
-                  id="input-group-1"
-                  label-cols-sm="4"
-                  label-cols-lg="3"
-                  label-for="avatar"
-          >
-            <b-avatar id="avatar" size="100px" :text=this.user.name.charAt(0)+this.user.lastname.charAt(0)></b-avatar>
-          </b-form-group>
+            <b-avatar id="avatar" :text=this.user.name.charAt(0)+this.user.lastname.charAt(0)></b-avatar>
+          <span class="mr-auto" :text=this.user.name></span>
+
           <b-form-group
                   id="input-group-3"
                   label-cols-sm="4"
@@ -21,42 +16,35 @@
                     id="startDate"
                     v-model="user.startDate"
                     dark
-                    placeholder="Seçiniz"
-                    today-button label-today-button="Bugün"
-                    reset-button label-reset-button="Sil"
-                    close-button label-close-button="Kapat"
                     :dateFormatOptions="{ year: 'numeric', month: 'numeric', day: 'numeric' }"
                     locale="tr"
                     aria-required="true"
                     :text=this.user.startDate
                     disabled
             ></b-form-datepicker>
+            <b-form-group
+                    id="input-group-4"
+                    label-cols-sm="4"
+                    label-cols-lg="3"
+                    label="Bitiş Tarihi :"
+                    label-for="endDate"
+            >
+              <b-form-datepicker
+                      id="endDate"
+                      v-model="user.endDate"
+                      dark
+                      :dateFormatOptions="{ year: 'numeric', month: 'numeric', day: 'numeric' }"
+                      locale="tr"
+                      required
+                      :text=this.user.endDate
+                      disabled
+              ></b-form-datepicker>
+            </b-form-group>
           </b-form-group>
 
         </b-col>
         <b-col class="col-6">
-          <b-form-group
-                  id="input-group-4"
-                  label-cols-sm="4"
-                  label-cols-lg="3"
-                  label="Bitiş Tarihi :"
-                  label-for="endDate"
-          >
-            <b-form-datepicker
-                    id="endDate"
-                    v-model="user.endDate"
-                    dark
-                    placeholder="Seçiniz"
-                    today-button label-today-button="Bugün"
-                    reset-button label-reset-button="Sil"
-                    close-button label-close-button="Kapat"
-                    :dateFormatOptions="{ year: 'numeric', month: 'numeric', day: 'numeric' }"
-                    locale="tr"
-                    required
-                    :text=this.user.endDate
-                    disabled
-            ></b-form-datepicker>
-          </b-form-group>
+
           <b-form-group
                   id="input-group-5"
                   label-cols-sm="4"
