c270abd3a41721d8e2a5e6d1749f359a0e5c8cea
onurgo <onurgokberk.ozerdal@nurd.com>
2020-05-13 01:55:50 +0300

    store data is permanently right now.

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import createPersistedState from 'vuex-persistedstate';
-import Cookies from 'js-cookie';
 
 Vue.use(Vuex);
 
@@ -10,13 +9,7 @@ const store = new Vuex.Store({
         username: null,
         userRole: null
     },
-    plugins: [createPersistedState({
-        storage: {
-            getItem: key => Cookies.get(key),
-            setItem: (key, value) => Cookies.set(key, value, { expires: 3, secure: true }),
-            removeItem: key => Cookies.remove(key)
-        }
-    })],
+    plugins: [createPersistedState()],
     mutations: {
         logout(state) {
             state.username = null;
