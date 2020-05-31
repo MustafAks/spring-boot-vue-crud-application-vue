b9477537539de24dc0fe44ed0e65ad3d9e282f02
onurgo <onurgokberk.ozerdal@nurd.com>
2020-05-31 19:27:39 +0300

    navbar responsive fix.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,25 +1,31 @@
 <template>
-    <b-navbar toggleable="md" style="background-color: #ec1b24;" type="dark">
-        <b-navbar-brand>
-            <img :src="require('../assets/images/NavbarImages/hasretNavbarImage.jpeg')" id='logo'>
-        </b-navbar-brand>
-        <b-navbar-toggle target="nav_ collapse"></b-navbar-toggle>
-        <b-collapse is-nav id="nav_collapse">
-            <b-navbar-nav>
-                <b-nav-item :to="{ name: 'mainPage'}" :active="$route.name == 'mainPage'">Anasayfa</b-nav-item>
-                <b-nav-item :hidden="this.$store.state.userRole !== 'ROLE_ADMIN' && this.$store.state.userRole !== 'ROLE_USER'" :to="{ name: 'Newspapers'}" :active="$route.name == 'Newspapers'">Gazeteler</b-nav-item>
-            </b-navbar-nav>
-            <b-navbar-nav class="ml-auto">
-                <b-nav-item :hidden="this.$store.state.userRole !== null" :to="{ name: 'login'}" :active="$route.name == 'login'">Giriş Yap</b-nav-item>
-                <b-nav-item-dropdown text="Profil" :hidden="this.$store.state.userRole === null">
-                    <b-dropdown-item :hidden="this.$store.state.userRole !== 'ROLE_USER'" :to="{ name: 'UserProfile'}" :active="$route.name == 'UserProfile'">Profil</b-dropdown-item>
-                    <b-dropdown-item :hidden="this.$store.state.userRole !== 'ROLE_ADMIN'" :to="{ name: 'AdminPanel'}" :active="$route.name == 'AdminPanel'">Admin Paneli</b-dropdown-item>
-                    <b-dropdown-item :hidden="this.$store.state.userRole === null" @click="logout()">Çıkış Yap</b-dropdown-item>
-                </b-nav-item-dropdown>
-                <b-nav-item :to="{ name: 'Contact'}" :active="$route.name == 'Contact'">İletişim</b-nav-item>
-            </b-navbar-nav>
-        </b-collapse>
-    </b-navbar>
+    <div>
+        <b-navbar toggleable="md" style="background-color: #ec1b24;" type="dark">
+            <b-navbar-brand>
+                <img :src="require('../assets/images/NavbarImages/hasretNavbarImage.jpeg')" id='logo'>
+            </b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav>
+                    <b-nav-item :to="{ name: 'mainPage'}" :active="$route.name == 'mainPage'">Anasayfa</b-nav-item>
+                    <b-nav-item :hidden="this.$store.state.userRole !== 'ROLE_ADMIN' && this.$store.state.userRole !== 'ROLE_USER'" :to="{ name: 'Newspapers'}" :active="$route.name == 'Newspapers'">Gazeteler</b-nav-item>
+                </b-navbar-nav>
+
+                <!-- Right aligned nav items -->
+                <b-navbar-nav class="ml-auto">
+                    <b-nav-item :hidden="this.$store.state.userRole !== null" :to="{ name: 'login'}" :active="$route.name == 'login'">Giriş Yap</b-nav-item>
+                    <b-nav-item-dropdown text="Profil" :hidden="this.$store.state.userRole === null">
+                        <b-dropdown-item :hidden="this.$store.state.userRole !== 'ROLE_USER'" :to="{ name: 'UserProfile'}" :active="$route.name == 'UserProfile'">Profil</b-dropdown-item>
+                        <b-dropdown-item :hidden="this.$store.state.userRole !== 'ROLE_ADMIN'" :to="{ name: 'AdminPanel'}" :active="$route.name == 'AdminPanel'">Admin Paneli</b-dropdown-item>
+                        <b-dropdown-item :hidden="this.$store.state.userRole === null" @click="logout()">Çıkış Yap</b-dropdown-item>
+                    </b-nav-item-dropdown>
+                    <b-nav-item :to="{ name: 'Contact'}" :active="$route.name == 'Contact'">İletişim</b-nav-item>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </div>
 </template>
 <script>
     export default {
