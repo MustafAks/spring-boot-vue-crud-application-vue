e4b22bfc2e6816aac63d556916702e8af479ec8a
onurgo <onurgokberk.ozerdal@nurd.com>
2020-06-12 15:05:23 +0300

    vue config

--- src/utils/AxiosUtils.js
+++ src/utils/AxiosUtils.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8085',
+  baseURL: 'http://localhost:8085/api',
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json',
@@ -22,7 +22,7 @@ axiosInstance.interceptors.response.use(
 );
 
 const axiosFileInstance = axios.create({
-  baseURL: 'http://localhost:8085',
+  baseURL: 'http://localhost:8085/api',
   timeout: 30000,
   headers: {
     'Content-Type': 'multipart/form-data'
