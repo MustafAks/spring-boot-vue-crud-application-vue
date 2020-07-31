68ec796c9b9c09bf984e04045e26f6d9760cb57a
onurozerdal <ozerdalonur@gmail.com>
2020-07-31 13:41:13 +0300

    google analystic code block

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import store from './store/Store'
 import VueGoodTablePlugin from 'vue-good-table';
 import 'vue-good-table/dist/vue-good-table.css'
 import "vuetify/dist/vuetify.css";
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
+import {BootstrapVue, BootstrapVueIcons} from 'bootstrap-vue';
+import VueAnalytics from 'vue-analytics';
 
 Vue.use(VueGoodTablePlugin);
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
+Vue.use(VueAnalytics, {id: 'UA-174171887-1', router});
 /* eslint-disable no-new */
-new Vue({ el: '#app', store, router, render: h => h(App) });
+new Vue({el: '#app', store, router, render: h => h(App)});
